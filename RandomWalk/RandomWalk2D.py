0f0a891a2ee4c3ccf52b4a37356b360b8580f309
TheEventHorizons <beyondtheeventhorizons@gmail.com>
2024-03-04 20:49:57 +0100

    add

--- RandomWalk/RandomWalk2D.py
+++ RandomWalk/RandomWalk2D.py
@@ -6,9 +6,12 @@ num_steps = 1000  # Nombre d'étapes
 step_size = 1  # Taille d'une étape
 x = 0
 y = 0
+x_coords = [x]
+y_coords = [y]
 
+np.random.seed(5)
 
-for i in range(3):
+for i in range(num_steps):
     p = np.random.randint(0, 4)
     print(p)
     
@@ -20,7 +23,16 @@ for i in range(3):
         y = y + step_size
     elif p == 3:
         y = y - step_size
-
     z = (x, y)
-    print(z)
+    x_coords.append(x)
+    y_coords.append(y)
+
+print(x,y)
+
+plt.figure(figsize=(12,8))
+plt.plot(x_coords, y_coords, marker='o', linestyle='-')
+plt.plot(0,0, marker= 'o', c='r')
+plt.plot(x,y, marker='o', c='black')
+plt.grid(ls='--')
+plt.show()
 
